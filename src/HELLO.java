2f3cd5e06515b9208abf47228804209dec40c5d9
vinod <vinodsaxena2100@gmail.com>
2022-11-02 12:57:45 +0530

    my computer

--- src/HELLO.java
+++ src/HELLO.java
@@ -6,11 +6,11 @@ public class HELLO
     public static void main (String[] args) 
     {
         
-        String str= "Geeks", nstr="";
+        String str= "gmail", nstr="";
         char ch;
         
       System.out.print("Original word: ");
-      System.out.println("Geeks"); //Example word
+      System.out.println("gmail"); //Example word
         
       for (int i=0; i<str.length(); i++)
       {
